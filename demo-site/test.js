dbc84803cba24f7913c4517f14581e62dec6d2ef
Matt Zeunert <matt@mostlystatic.com>
2013-12-30 16:05:39 +0000

    Implement logging variable declarations.

--- demo-site/test.js
+++ demo-site/test.js
@@ -1,6 +1,6 @@
+
 function testMethod(test){
-	var a;
-	a = 2;
+	var a = 1 + 1;
 	
 	console.log(a)
 	
@@ -13,6 +13,17 @@ function testMethod(test){
 
 }
 
+// Todo: turn these into specs
+// need to break up add into two functions, return statement handling and param handling
+
 function add(a, b){
 	return a + b;
+}
+
+function singleVar(){
+	var a = 5;
+}
+
+function multiVar(){
+	var a = 1, b = 2,c = 3;
 }
