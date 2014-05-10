c9c29f0bff4bddb6a210e55eb97c05d62b91c977
Matt Zeunert <matt@mostlystatic.com>
2014-05-10 18:09:52 +0100

    Add results server.

---
+++ result-server/server.js
@@ -0,0 +1,30 @@
+/*
+    Serves annotation results so viewed code can be annoted with the previously collected data.
+*/
+
+var express = require('express');
+var fs = require('fs');
+var app = express();
+app.use(express.bodyParser());
+
+// Endpoint to send logging data to
+app.get(/\/results\/.*/, function(req, res){
+    var path = req.path;
+    var fileHash = req.path.replace('/results/', '');
+
+    var resultsFilePath = './results/' + fileHash + '.json';
+    fs.exists(resultsFilePath, function(exists) {
+        if (exists) {
+            fs.readFile(resultsFilePath, function(err, data){
+                res.end(data.toString());
+            })
+        } else {
+            res.end(JSON.stringify({
+                success: false,
+                message: 'No results found.'
+            }));
+        }
+    });
+});
+
+app.listen(7001);
