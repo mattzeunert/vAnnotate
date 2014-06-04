586a33ab99cd620bf6993f3347005661e15d7de0
Matt Zeunert <matt@mostlystatic.com>
2014-06-04 21:43:46 +0100

    Integrate merging.

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -5,16 +5,30 @@ var repos = [
     'jashkenas/underscore'
 ]
 
+var scrapingPath = process.env['HOME'] + '/vAScraping/';
+var unzipPath = scrapingPath + 'unzip/';
+var resultPath = scrapingPath + 'individual-results/';
+var mergedResultPath = scrapingPath + 'results/'
+
+var mkdirp = require('mkdirp');
+mkdirp(scrapingPath)
+mkdirp(unzipPath)
+mkdirp(resultPath)
+mkdirp(mergedResultPath)
+
+
 var i = 0;
+
 continueProcessing();
+
+
 
 function continueProcessing(){
     var https = require('https');
     var fs = require('fs');
 
-    require('mkdirp')('/tmp/vAScraping/')
     var repoId = repos[i].replace(/\//g, '-').replace(/[^a-z0-9A-Z-_]/g,'') + '-' + new Date().toISOString().replace(/\:/g, '-')
-    var scraperRoot = '/tmp/vAScraping/';
+    var scraperRoot = unzipPath
     var filePath = scraperRoot + repoId + '.zip'
     var file = fs.createWriteStream(filePath);
     var url = 'https://codeload.github.com/' + repos[i] + '/zip/master';
@@ -23,18 +37,36 @@ function continueProcessing(){
         response.pipe(file);
         file.on('finish', function() {
           console.log('Finished fetching')
-          var cmd = 'unzip ' + filePath + ' -d ' + scraperRoot + repoId
+          var unzipDirectory = scraperRoot + repoId
+          var cmd = 'unzip ' + filePath + ' -d ' + unzipDirectory
           console.log(cmd)
           exec(cmd, function(){
             console.log('unzipped')
+            cmd = 'node lib/vAnnotate ' + unzipDirectory;
+            cmd += ' --htmlFileV2=ALL --outputDirectoryV2=' + resultPath;
+            exec(cmd, function(){
+                i++;
+                if (i==repos.length){
+                    merge();
+                }
+                else {
+                    continueProcessing();
+                }
+            })
+
+
           })
         });
     });
-
-
-    // exec('node --version', function callback(error, stdout, stderr){
-    //     console.log(stdout, stderr)
-    // });
-
 }
+
+function merge(){
+    var cmd = "node lib/vAnnotate --mergeResultDirectory=" + resultPath + " --mergeResultOutputDirectory=" + mergedResultPath;
+    exec(cmd, function(){
+        console.log(arguments)
+        console.log('---DONE----')
+    })
+}
+
+
 
